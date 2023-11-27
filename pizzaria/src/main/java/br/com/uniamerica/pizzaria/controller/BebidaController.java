ead7ed8406c6a62a94aa59f85ac99115d396b82f
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-11-27 20:33:03 -0300

    Criação das Controllers e do projeto Angular

--- pizzaria/src/main/java/br/com/uniamerica/pizzaria/controller/BebidaController.java
+++ pizzaria/src/main/java/br/com/uniamerica/pizzaria/controller/BebidaController.java
@@ -1,4 +1,147 @@
 package br.com.uniamerica.pizzaria.controller;
 
+import br.com.uniamerica.pizzaria.dto.BebidaDTO;
+import br.com.uniamerica.pizzaria.dto.atualizar.BebidaAtualizarDTO;
+import br.com.uniamerica.pizzaria.dto.cadastro.BebidaCadastroDTO;
+import br.com.uniamerica.pizzaria.entity.Bebida;
+import br.com.uniamerica.pizzaria.service.BebidaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bebida")
+@CrossOrigin(origins = "*")
 public class BebidaController {
+    private final BebidaService bebidaService;
+    @Autowired
+    public BebidaController(BebidaService bebidaService) {
+        this.bebidaService = bebidaService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BebidaDTO> findById(@PathVariable Long id) {
+        return bebidaService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BebidaDTO>> findAll() {
+        List<BebidaDTO> bebidaDTOS = bebidaService.findAll();
+        return ResponseEntity.ok(bebidaDTOS);
+    }
+
+    @GetMapping("/nome/{nomeBebida}")
+    public ResponseEntity<BebidaDTO> findByName(@PathVariable String nomeBebida) {
+        try{
+            BebidaDTO bebidaDTO = bebidaService.findByName(nomeBebida);
+
+            if (bebidaDTO != null){
+                return ResponseEntity.ok(bebidaDTO);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("ativo/{ativo}")
+    public ResponseEntity<List<BebidaDTO>> findByAtivo(@PathVariable boolean ativo) {
+        try {
+            List<BebidaDTO> bebidaDTOS = bebidaService.findByAtivo(ativo);
+
+            if (!bebidaDTOS.isEmpty()) {
+                return ResponseEntity.ok(bebidaDTOS);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("registro/dia/{registro}")
+    public ResponseEntity<List<BebidaCadastroDTO>> findByDiaRegistro(@PathVariable("registro") LocalDate registro) {
+        try {
+            List<BebidaCadastroDTO> bebidaDTOS = bebidaService.findByDiaRegistro(registro);
+
+            if (!bebidaDTOS.isEmpty()) {
+                return ResponseEntity.ok(bebidaDTOS);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("atualizar/dia/{atualizar}")
+    public ResponseEntity<List<BebidaAtualizarDTO>> findByDiaAtualizar(@PathVariable("atualizar") LocalDate atualizar) {
+        try {
+            List<BebidaAtualizarDTO> bebidaDTOS = bebidaService.findByDiaAtualizar(atualizar);
+
+            if (!bebidaDTOS.isEmpty()) {
+                return ResponseEntity.ok(bebidaDTOS);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<String> cadastrarBebida(@RequestBody Bebida bebida) {
+        try {
+            bebidaService.cadastrar(bebida);
+            return ResponseEntity.ok().body("Registro cadastrado com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> atualizarBebida(@PathVariable Long id, @RequestBody Bebida bebida) {
+        try {
+            bebidaService.atualizar(id, bebida);
+            return ResponseEntity.ok().body("Registro atualizado com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarBebida(@PathVariable Long id) {
+        try {
+            bebidaService.deleteBebida(id);
+            return ResponseEntity.ok("Registro excluído com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/desativar/{id}")
+    public ResponseEntity<String> deletar(@PathVariable Long id) {
+        try {
+            bebidaService.desativar(id);
+            return ResponseEntity.ok().body("Registro desativado com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao desativar o registro.");
+        }
+    }
+
+    @GetMapping("erro")
+    private ResponseEntity<List<BebidaDTO>> exemploErro(){
+        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+
 }
