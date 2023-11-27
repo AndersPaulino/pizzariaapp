85cbcf69441f52d92cbf05db4aa79102fa074c87
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-11-27 14:00:20 -0300

    Criação da Classe ClienteRepository.

---
+++ pizzaria/src/main/java/br/com/uniamerica/pizzaria/repository/ClienteRepository.java
@@ -0,0 +1,29 @@
+package br.com.uniamerica.pizzaria.repository;
+
+import br.com.uniamerica.pizzaria.entity.Cliente;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    @EntityGraph(attributePaths = "endereco")
+    Cliente findByNome(String nome);
+
+    @EntityGraph(attributePaths = "endereco")
+    Cliente findByCpf(String cpf);
+
+    @Query("SELECT e FROM Cliente e WHERE e.ativo = :ativo")
+    public List<Cliente> findByAtivo(@Param("ativo") boolean ativo);
+
+    @Query("SELECT e FROM Cliente e WHERE DATE(e.registro) = :registro")
+    List<Cliente> findByDiaRegistro(@Param("registro") LocalDate registro);
+
+    @Query("SELECT e FROM Cliente e WHERE DATE(e.atualizar) = :atualizar")
+    List<Cliente> findByDiaAtualizar(@Param("atualizar") LocalDate atualizar);
+}
