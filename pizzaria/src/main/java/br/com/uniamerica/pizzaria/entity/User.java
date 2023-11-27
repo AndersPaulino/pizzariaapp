9a7e730e1f460b2cc4297dd18c93354636391330
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-11-27 11:14:38 -0300

    Criação das configurações de segurança do projeto Pizzaria.

---
+++ pizzaria/src/main/java/br/com/uniamerica/pizzaria/entity/User.java
@@ -0,0 +1,59 @@
+package br.com.uniamerica.pizzaria.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user",schema = "public")
+public class User extends AbsctractEntity implements UserDetails {
+
+    @Getter
+    @Setter
+    @Column(name = "cl_name")
+    private String username;
+
+    @Getter @Setter
+    @Column(name = "cl_password")
+    private String password;
+
+    @Getter @Setter
+    @Column(name = "cl_role")
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> auth = new ArrayList<>();
+        auth.add(new SimpleGrantedAuthority(this.role));
+        return auth;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ pizzaria/src/test/java/br/com/uniamerica/pizzaria/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
