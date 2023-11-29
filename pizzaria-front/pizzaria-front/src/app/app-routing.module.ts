47006020ef4eff8a26dfc78654f11da1c601d71a
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-11-28 23:26:43 -0300

    Funcionario

--- pizzaria-front/pizzaria-front/src/app/app-routing.module.ts
+++ pizzaria-front/pizzaria-front/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { IndexComponent } from './components/layout/index/index.component';
 import { BebidasListComponent } from './components/bebida/bebidalist/bebidalist.component';
 import { EnderecolistComponent } from './components/endereco/enderecolist/enderecolist.component';
 import { SaborlistComponent } from './components/sabor/saborlist/saborlist.component';
+import { FuncionariolistComponent } from './components/funcionario/funcionariolist/funcionariolist.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "login", pathMatch: 'full' },
@@ -13,7 +14,8 @@ const routes: Routes = [
     path: "admin", component: IndexComponent, children: [
       { path: "bebida", component: BebidasListComponent },
       { path: "endereco", component: EnderecolistComponent },
-      { path: "sabor", component: SaborlistComponent }
+      { path: "sabor", component: SaborlistComponent },
+      { path: "funcionario", component:FuncionariolistComponent  }
 ]}
 
 ];
--- pizzaria-front/pizzaria-front/src/app/components/funcionario/funcionariolist/funcionariolist.component.ts
+++ pizzaria-front/pizzaria-front/src/app/components/funcionario/funcionariolist/funcionariolist.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { NgbModal , NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Funcionario } from 'src/app/models/funcionario/funcionario';
+import { FuncionarioService } from 'src/app/services/funcionario/funcionario.service';
 
 @Component({
   selector: 'app-funcionariolist',
@@ -7,4 +10,76 @@ import { Component } from '@angular/core';
 })
 export class FuncionariolistComponent {
 
+  lista: Funcionario[] = [];
+
+  funcionarioSelecionadoParaEdicao: Funcionario = new Funcionario();
+  indiceSelecionadoParaEdicao!: number;
+
+  @Output() retorno = new EventEmitter<Funcionario>();
+  @Input() modoLancamento: boolean = false;
+
+  modalService = inject(NgbModal);
+  funcionarioService = inject(FuncionarioService);
+
+  constructor() {
+    this.listAll();
+  }
+
+  listAll() {
+    this.funcionarioService.listAll().subscribe({
+      next: lista => {
+        this.lista = lista;
+      },
+      error: erro => {
+        alert('Observe o erro no console!');
+        console.error(erro);
+      }
+    });
+  }
+
+  exemploErro() {
+    this.funcionarioService.exemploErro().subscribe({
+      next: lista => { // QUANDO DÁ CERTO
+        this.lista = lista;
+      },
+      error: erro => { // QUANDO DÁ ERRO
+        alert('Observe o erro no console!');
+        console.error(erro);
+      }
+    });
+  }
+  
+  adicionar(modal: any) {
+    this.funcionarioSelecionadoParaEdicao = new Funcionario();
+    this.modalService.open(modal, { size: 'sm' });
+  }
+
+  editar(modal: any, funcionario: Funcionario, indice: number) {
+    this.funcionarioSelecionadoParaEdicao = { ...funcionario };
+    this.indiceSelecionadoParaEdicao = indice;
+    this.modalService.open(modal, { size: 'sm' });
+  }
+
+  addOuEditarFuncionario(funcionario: Funcionario) {
+    const onComplete = () => {
+      this.listAll();
+      this.modalService.dismissAll();
+    };
+
+    if (funcionario.id) {
+      console.log("Aqui foi atualizar");
+      this.funcionarioService.atualizarFuncionario(funcionario.id, funcionario).subscribe(onComplete);
+    } else {
+      console.log("Aqui foi cadastrar");
+      this.funcionarioService.cadastrarFuncionario(funcionario).subscribe(onComplete);
+    }
+  }
+
+  deletar(id: number) {
+    this.funcionarioService.deletarFuncionario(id).subscribe(() => this.listAll());
+  }
+
+  lancamento(funcionario: Funcionario){
+    this.retorno.emit(funcionario);
+  }
 }
