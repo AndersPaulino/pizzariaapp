88c38af5f69d4b854a7f45a10f077f5bcee5df14
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-11-30 13:19:02 -0300

    50% Statements

--- pizzaria-front/pizzaria-front/src/app/components/bebida/bebidalist/bebidalist.component.spec.ts
+++ pizzaria-front/pizzaria-front/src/app/components/bebida/bebidalist/bebidalist.component.spec.ts
@@ -3,7 +3,7 @@ import { BebidaListComponent } from './bebidalist.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModal, NgbModalModule, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { BebidaService } from 'src/app/services/bebida/bebida.service';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 
 describe('BebidaListComponent', () => {
@@ -50,4 +50,24 @@ describe('BebidaListComponent', () => {
     component.lancamento(bebida);
     expect(component.retorno.emit).toHaveBeenCalledWith(bebida);
   });
+
+  it('should handle listAll success', fakeAsync(() => {
+    const bebida = { id: 1, nomeBebida: 'Coca-Cola', valorBebida: 2.5, ativo: true, registro: new Date(), atualizar: new Date() };
+    spyOn(bebidaService, 'listAll').and.returnValue(of([bebida]));
+
+    component.listAll();
+    tick();
+
+    expect(component.lista).toEqual([bebida]);
+}));
+
+  it('should handle listAll error', fakeAsync(() => {
+    spyOn(bebidaService, 'listAll').and.returnValue(throwError('Erro ao buscar bebidas'));
+
+    component.listAll();
+    tick();
+
+    expect(component.lista).toEqual([]);
+    // You can also test if the error handling logic is working as expected
+  }));
 });
