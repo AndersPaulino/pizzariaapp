08618c892a1c6bc844f7c7ef61d7321f7d42a422
Anderson Paulino <90426252+AndersPaulino@users.noreply.github.com>
2023-11-30 22:33:51 -0300

    Correções.

--- pizzaria-front/pizzaria-front/src/app/components/login/login.component.ts
+++ pizzaria-front/pizzaria-front/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Login } from 'src/app/models/login/login';
-import { LoginService } from 'src/app/services/login/login.service';
+import { Usuario } from 'src/app/models/usuario/usuario';
 
 @Component({
   selector: 'app-login',
@@ -9,43 +9,19 @@ import { LoginService } from 'src/app/services/login/login.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  usuario: Usuario = new Usuario();
   login: Login = new Login();
   roteador = inject(Router);
-  loginService = inject(LoginService);
-
+  
   constructor() {
-    this.loginService.removerToken();
+    
   }
 
   logar() {
-
-
-    this.loginService.logar(this.login).subscribe({
-      next: usuario => { // QUANDO DÁ CERTO
-        console.log(usuario);
-        this.loginService.addToken(usuario?.token);
-        this.roteador.navigate(['admin']);
-      },
-      error: erro => { // QUANDO DÁ ERRO
-        alert('Exemplo de tratamento de erro/exception! Observe o erro no console!');
-        console.error(erro);
-      }
-    });
-
-    if (this.login.username == 'admin' && this.login.password == 'admin')
-      this.roteador.navigate(['admin']);
-    else
-      alert('login ou senha incorretos');
-
-  } 
-
- /*logar() {
-    if (this.login.username == 'admin' && this.login.password == 'admin')
+    if (this.usuario.login == 'admin' && this.usuario.senha == 'admin')
       this.roteador.navigate(['admin/bebida']);
     else
       alert('Login ou senha incorretos!');
-  }*/
+  }
 
-  
 }
